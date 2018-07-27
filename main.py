32369db07f2ecbfa6a4055ca3dd69aeda1369ac5
RyanRio <thermitchellman@gmail.com>
2018-07-27 08:34:54 -0400

    update gitignore to ignore .vscode and minor syntax fix to previous commit

--- main.py
+++ main.py
@@ -209,11 +209,11 @@ async def on_server_role_update(role):
     # now can message user to determine if game role
     msg = await client.send_message(client.get_channel('451532020695433217'), 'React with check mark if ' + role.name + ' is a game role, x if it is not a game role')
     res = await client.wait_for_reaction(['✅', '❌'], message=msg)
-    if(res.reaction.emoji===✅):
+    if(res.reaction.emoji=='✅'):
         # role is a game role
         role_msg = client.get_message(client.get_channel('451532020695433217'), '451547972161896448')
         split = role_msg.content.split("You can remove roles")
-        let new_msg = split[0] + f"`.iam {role.name}`\n" + "You can remove roles with `.iamnot <game>`"
+        let new_msg = split[0] + f"`.iam {role.name}`\n" + "You can remove roles with `.iamnot <game>`")
         client.edit_message(role_msg, new_msg)
 
 @client.event
