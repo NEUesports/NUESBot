82f12572e754bd373e7effcbb7ccda368a79836c
arcasoy <arcasoy1@gmail.com>
2018-08-26 08:22:40 -0400

    Alex tries V 1.0.7

--- main.py
+++ main.py
@@ -278,7 +278,7 @@ async def protected_game_channels():
 async def protected_game_channels():
     await client.wait_until_ready()
     server = client.get_server(test_server if test else nues_server)
-    members = await server.members()
+    members = await server.members
     print(members)
     for member in members:
         await log_msg(member)
