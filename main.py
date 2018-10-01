53e41a89b9f90e02993f6d638cbdfd1de0868caf
Sciguymjm <sciguymjm@gmail.com>
2018-10-01 16:38:20 -0400

    remove game roles

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import asyncio
+import json
 import logging
 
 import discord
 import gspread
-import json
 from oauth2client.service_account import ServiceAccountCredentials
 
 from creds import DISCORD_API_KEY
@@ -44,6 +44,7 @@ with open('game_roles.json') as f:
 
 server = client.get_server(test_server if test else nues_server)
 
+
 @client.event
 async def on_ready():
     print('------')
@@ -53,6 +54,7 @@ async def on_ready():
     print('------')
     print('Current Game Roles:')
     print(game_roles)
+
 
 def has_role(user, role_name):
     return any([r.name.lower() == role_name.lower() for r in user.roles])
@@ -72,6 +74,7 @@ async def log_msg(msg):
     logger.info(f'{msg}')
     return await client.send_message(discord.Object(test_log if test else nues_log), msg)
 
+
 @client.event
 async def on_message(message: discord.Message):
     if message.content.startswith('!test') and has_role(message.author, 'Student'):
@@ -89,6 +92,8 @@ async def on_message(message: discord.Message):
         await send_welcome(message.author)
     elif message.content.startswith('!join'):
         await on_member_join(message.author)
+    elif message.content.startswith('!remove') and any([r.name == 'Bot Master' for r in message.author.roles]):
+        remove_game_role(message.content.replace('!remove ', ''))
     elif message.channel.name == 'set-roles':
         if message.content.startswith('.iam ') and any([r.name == 'Student' for r in message.author.roles]):
             logger.info(f'User {message.author} requesting role change')
@@ -120,6 +125,7 @@ async def on_message(message: discord.Message):
             m = buildGRMsg()
             await client.send_message(message.channel, m)
 
+
 # build the game roles message
 def buildGRMsg():
     m = "Available roles:\n"
@@ -129,9 +135,11 @@ def buildGRMsg():
     m += "You can remove roles with `.iamnot <game>`"
     return m
 
+
 async def dontcrash():
     channels = client.get_all_channels()
     asyncio.sleep(50)
+
 
 @client.event
 async def on_member_join(user: discord.Member):
@@ -144,6 +152,7 @@ async def on_member_join(user: discord.Member):
     except Exception as e:
         await log_msg(f'Could not send welcome message to {user.mention}! {e}')
 
+
 async def poll_sheet():
     await client.wait_until_ready()
     await client.change_presence(game=discord.Game(name='Join OrgSync!'))
@@ -155,7 +164,7 @@ async def poll_sheet():
             server = client.get_server(test_server if test else nues_server)
             try:
                 ppl = sheet.col_values(3)[1:][::-1]
-                emails = sheet.col_values(2)[1:][::-1] # reverse so we use the most recent
+                emails = sheet.col_values(2)[1:][::-1]  # reverse so we use the most recent
                 first_names = sheet.col_values(6)[1:][::-1]
                 ingame_names = sheet.col_values(7)[1:][::-1]
             except gspread.exceptions.APIError:
@@ -176,10 +185,10 @@ async def poll_sheet():
                     continue
                 usr = server.get_member_named(discord_username)
                 if usr is None:
-                    print(f'User {discord_username} does not exist!')
                     if ' #' in discord_username:
                         usr = server.get_member_named(discord_username.replace(' #', '#'))
                     if usr is None:
+                        print(f'User {discord_username} does not exist!')
                         continue
                 if has_role(usr, 'Student'):
                     pass
@@ -202,65 +211,77 @@ async def poll_sheet():
         except Exception as e:
             await log_msg(f'Error checking spreadsheet: {e}')
 
+
 async def send_welcome(user: discord.Member):
     welcome_message = sheet2.col_values(1)[1]
     await client.send_message(user, welcome_message)
     logger.info(f'Sent welcome message to {user}')
 
 
-#Ask if newly creted role is game role.
+# Ask if newly creted role is game role.
 @client.event
-async def on_server_role_create(new_role):
+async def on_server_role_create(new_role: str):
     if new_role.name != 'new role':
         server = client.get_server(test_server if test else nues_server)
-        exec_board_role = discord.utils.get(server.roles, name = "Executive Board", id = test_execboard if test else nues_execboard)
+        exec_board_role = discord.utils.get(server.roles, name="Executive Board",
+                                            id=test_execboard if test else nues_execboard)
         new_gamerole_msg = (exec_board_role.mention + " is " + new_role.name + " a game role?")
         new_gamerole_msg = await log_msg(new_gamerole_msg)
         await client.add_reaction(new_gamerole_msg, '✅')
         await client.add_reaction(new_gamerole_msg, '❌')
         await asyncio.sleep(1)
-        res = await client.wait_for_reaction(['✅', '❌'], message= new_gamerole_msg)
+        res = await client.wait_for_reaction(['✅', '❌'], message=new_gamerole_msg)
         await log_msg("Thank you for your feedback!")
-        if(res.reaction.emoji=='✅'):
-            #add the game role to the game_roles list
+        if (res.reaction.emoji == '✅'):
+            # add the game role to the game_roles list
             game_roles.append(new_role.name)
-            #build a new GRMsg and edit the old one with the new one
-            set_roles_channel = client.get_channel('465609299285245955' if test else '451532020695433217')
-            role_msg = await client.get_message(set_roles_channel, '482608179104972820' if test else '451547972161896448')
-            new_GRmsg = buildGRMsg()
-            await client.edit_message(role_msg, new_GRmsg)
+            # build a new GRMsg and edit the old one with the new one
+            update_gm_message()
             await log_msg("Updated Set Roles message with " + new_role.name)
-
             with open("game_roles.json", mode="w") as f:
                 json.dump(game_roles, f)
         await client.delete_message(new_gamerole_msg)
 
-#ask if role is a game role if the name is updated from "new role"
+
+# ask if role is a game role if the name is updated from "new role"
 @client.event
 async def on_server_role_update(new_role_prename, new_role_postname):
     if new_role_prename.name == 'new role':
         if new_role_prename.name != new_role_postname.name:
             server = client.get_server(test_server if test else nues_server)
-            exec_board_role = discord.utils.get(server.roles, name = "Executive Board", id = test_execboard if test else nues_execboard)
+            exec_board_role = discord.utils.get(server.roles, name="Executive Board",
+                                                id=test_execboard if test else nues_execboard)
             new_gamerole_msg = (exec_board_role.mention + " is " + new_role_postname.name + " a game role?")
             new_gamerole_msg = await log_msg(new_gamerole_msg)
             await client.add_reaction(new_gamerole_msg, '✅')
             await client.add_reaction(new_gamerole_msg, '❌')
             await asyncio.sleep(1)
-            res = await client.wait_for_reaction(['✅', '❌'], message= new_gamerole_msg)
+            res = await client.wait_for_reaction(['✅', '❌'], message=new_gamerole_msg)
             await log_msg("Thank you for your feedback!")
-            if(res.reaction.emoji=='✅'):
-                #add the game role to the game_roles list
+            if (res.reaction.emoji == '✅'):
+                # add the game role to the game_roles list
                 game_roles.append(new_role_postname.name)
-                #build a new GRMsg and edit the old one with the new one
-                set_roles_channel = client.get_channel('465609299285245955' if test else '451532020695433217')
-                role_msg = await client.get_message(set_roles_channel, '482608179104972820' if test else '451547972161896448')
-                new_GRmsg = buildGRMsg()
-                await client.edit_message(role_msg, new_GRmsg)
+                update_gm_message()
                 await log_msg("Updated Set Roles message with " + new_role_postname.name)
                 with open("game_roles.json", mode="w") as f:
                     json.dump(game_roles, f)
             await client.delete_message(new_gamerole_msg)
+
+
+async def update_gm_message():
+    set_roles_channel = client.get_channel('465609299285245955' if test else '451532020695433217')
+    role_msg = await client.get_message(set_roles_channel, '482608179104972820' if test else '451547972161896448')
+    new_GRmsg = buildGRMsg()
+    await client.edit_message(role_msg, new_GRmsg)
+
+
+async def remove_game_role(role_name: str):
+    try:
+        game_roles.remove(role_name)
+        update_gm_message()
+        await log_msg('Removed game role ' + role_name)
+    except:
+        await log_msg('Error removing game role')
 
 
 async def protected_game_channels():
@@ -272,12 +293,13 @@ async def protected_game_channels():
     for member in members:
         for game_role in game_roles:
             if has_role(member, game_role) and not has_role(member, 'Student'):
-                role = discord.utils.get(server.roles, name = f'{game_role}')
+                role = discord.utils.get(server.roles, name=f'{game_role}')
                 await client.remove_roles(member, role)
-                rem_role_msg = (f'{member} your role, {game_role}, on the Northeastern University Esports Discord has been removed since you are not registered as a student. Please register as a student using the Google Form at https://goo.gl/forms/AwC3tuYLg0GQMPYs1 to regain access to game roles.')
+                rem_role_msg = (
+                    f'{member} your role, {game_role}, on the Northeastern University Esports Discord has been removed since you are not registered as a student. Please register as a student using the Google Form at https://goo.gl/forms/AwC3tuYLg0GQMPYs1 to regain access to game roles.')
                 await client.send_message(member, rem_role_msg)
                 await log_msg(f'Removed `{game_role}` role from `{member}`')
-    await asyncio.sleep(86400) #task runs once a day
+    await asyncio.sleep(86400)  # task runs once a day
 
 
 client.loop.create_task(protected_game_channels())
